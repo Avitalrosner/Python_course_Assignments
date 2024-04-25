de434e14393865a32260b872054b32d59e58d495
Abi Rose <abirose134@gmail.com>
2024-04-25 18:42:49 +0300

    Add second assignment code

---
+++ assignment_2.py
@@ -0,0 +1,10 @@
+first = input("first num: ")
+second = input("second num: ")
+sum = first + second
+print(f"the sum is {sum}")
+
+lang = input("what: ")
+if lang == "python":
+    print('yup')
+else:
+    print('nope')
