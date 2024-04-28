f1e24ebe03bb50e403a9f760bbe5929452fbbb8a
Avital Rosner <166692736+Avitalrosner@users.noreply.github.com>
2024-04-28 15:57:11 +0300

    assignment.py

--- Day2/assignment.py
+++ Day2/assignment.py
@@ -9,19 +9,19 @@ num_of_guess = 0
 while True:
     # The user guesses a number:
     user_guess = input("Please enter a number: ")
+    num_of_guess += 1
     # The computer tells if the guess was too small, too big or exact.
     # If exact the user wins, the program ends. Otherwise the user guesses again.
-
-    if int(user_guess) > int(computer_num):
+    
+    if int(user_guess) > computer_num:
         print("Too big")
-        num_of_guess += 1
-    elif int(user_guess) < int(computer_num):
+    elif int(user_guess) < computer_num:
         print("Too small")
-        num_of_guess += 1
     else:
         print("You win")
-        num_of_guess += 1
         # print how many guesses the user needed.
         print(f"The number of guesses are {num_of_guess}")
         break
+
         
+                
