140112d54f28ca2bbea856430a3274444c63832f
Avital Rosner <166692736+Avitalrosner@users.noreply.github.com>
2024-05-26 15:35:31 +0300

    process_umi.py

--- Day6/process_umi.py
+++ Day6/process_umi.py
@@ -1,1 +1,18 @@
+#Defining functions for loading the data and calculating the log10 for the sum of UMI's:
+
+import pandas as pd
+import numpy as np
+
+def load_data(file_path):
+    return pd.read_csv(file_path)
+
+def calculate_log10(data, column_name):
+    data['log10_' + column_name] = np.log10(data[column_name])
+    return data
+
+if __name__ == "__main__":
+    file_path = "C:python......csv"
+    data = load_data(file_path)
+    data = calculate_log10(data, 'sum_of_umis')
+    print(data)
 
