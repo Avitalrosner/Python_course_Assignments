ab0383461bb8c1d7650cb5cdce36fd64d9ab2149
Abi Rose <abirose134@gmail.com>
2024-05-27 21:41:38 +0300

    Add process_umi.py and test_process_umi.py to Day6 folder

---
+++ Day6/test_process_umi.py
@@ -0,0 +1,25 @@
+#Testing that the processing works:
+
+import unittest
+import pandas as pd
+from process_umi import load_data, calculate_log10
+
+class TestProcessUmi(unittest.TestCase):
+
+    def setUp(self):
+        self.file_path = 'C:/python/Python_course_Assignments/Day6/test_umi.csv'
+        self.data = load_data(self.file_path)
+
+    def test_load_data(self):
+        self.assertEqual(len(self.data), 3)
+        self.assertEqual(list(self.data.columns), ['patient', 'cancer', 'sum_of_umis'])
+
+    def test_calculate_log10(self):
+        expected_log10_values = pd.Series([4.0, 3.0, 2.0], name='log10_sum_of_umis')
+        self.data = calculate_log10(self.data, 'sum_of_umis')
+        calculated_log10_values = self.data['log10_sum_of_umis']
+        pd.testing.assert_series_equal(calculated_log10_values, expected_log10_values)
+
+if __name__ == "__main__":
+    unittest.main()
+
